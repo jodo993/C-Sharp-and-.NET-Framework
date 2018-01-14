a28ad6eeae6e8f87b4b8e642cba0a8ede94c7ac3
jodo993 <josephdo993@yahoo.com>
2018-01-13 22:35:46 -0800

    Chapter 2 Rocking App
    - testing background and foreground colors nothing big

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocking_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Welcome to Rock";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("------------------------");
+            Console.WriteLine("     ROCK ROCK ROCK     ");
+            Console.WriteLine("------------------------");
+            Console.BackgroundColor = ConsoleColor.White;
+
+            Console.ReadLine();
+        }
+    }
+}
