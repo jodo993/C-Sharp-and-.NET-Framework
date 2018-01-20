b40b085f89853a16fc9243458a338aa2b019262c
jodo993 <josephdo993@yahoo.com>
2018-01-20 02:43:58 -0800

    Dictionary Search Comparison 1.0
    - used dictionary 
    - code similar to hashtable one uploaded earlier

--- Program.cs
+++ Program.cs
@@ -4,46 +4,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Diagnostics;
 
 namespace HashTableSearch
 {
     class Program
     {
-        static Hashtable userSearchHashTable;
+        //static Hashtable userSearchHashTable;
+        //static Stopwatch sw;
 
         static void Main(string[] args)
         {
-            userSearchHashTable = new Hashtable();
+            //// Create hashtable
+            //userSearchHashTable = new Hashtable();
+            //sw = new Stopwatch();
 
-            Console.WriteLine("Type in a sentence: ");
-            string sentence = Console.ReadLine();
+            //// Create timer
+            //float startTime = 0;
+            //float endTime = 0;
+            //float elapsedTime = 0;
 
-            Console.WriteLine("Total Words: " + sentence.Split().Length);
+            //Console.WriteLine("Type in a sentence: ");
+            //string sentence = Console.ReadLine();
 
-            int i = 1;
-            foreach (string word in sentence.Split())
-            {
-                if (userSearchHashTable.Contains(word))
-                    userSearchHashTable[word] = i + 1;
-                else
-                {
-                    userSearchHashTable.Add(word, i);
-                }
-            }
+            //startTime = sw.ElapsedTicks;
 
-            foreach (DictionaryEntry entry in userSearchHashTable)
-            {
-                Console.WriteLine("Word: " + entry.Key + " Appearance: " + entry.Value);
-            }
+            //Console.WriteLine("Total Words: " + sentence.Split().Length);
 
+            //int i = 0;
+            //foreach (string word in sentence.Split())
+            //{
+            //    if (userSearchHashTable.Contains(word))
+            //    {
+            //        userSearchHashTable[word] = i;
+            //    }  
+            //    else
+            //        userSearchHashTable.Add(word, i);
+            //}
+
+            //foreach (DictionaryEntry entry in userSearchHashTable)
+            //{
+            //    Console.WriteLine("Word: " + entry.Key + " Appearance: " + entry.Value);
+            //}
+
+            //double totalMatched = 0;
             //string matchOne = "My computer wont turn on";
             //string matchTwo = "Chromebook screen is black";
             //string matchThree = "Lexia keeps asking for cookies";
 
-            //foreach(DictionaryEntry word in userSearchHashTable)
+            //foreach (string word in matchOne.Split())
             //{
-            //    if ()
+            //    if (userSearchHashTable.ContainsKey(word))
+            //    {
+            //        //userSearchHashTable[word] = +1;
+            //        //totalMatched = totalMatched + 2;
+            //    }        
+            //    else
+            //        userSearchHashTable[word] = +0;
             //}
+
+            //endTime = sw.ElapsedTicks;
+            //elapsedTime = endTime - startTime;
+            //Console.WriteLine("");
+
+            //foreach (DictionaryEntry entry in userSearchHashTable)
+            //{
+            //    Console.WriteLine("Word: " + entry.Key + " Appearance: " + entry.Value);
+            //}
+
+            //Console.WriteLine("Total Time: " + string.Format("{0:0.##}", elapsedTime) + " ms.");
+            //Console.WriteLine("Total Matched: " + totalMatched);
+
+
+            Dictionary<string, int> input = new Dictionary<string, int>();
+
+            Console.WriteLine("Write: ");
+            string phrase = Console.ReadLine();
+            int totalWord = phrase.Split().Length;
+            string test = "computer wont turn on on on";
+
+            foreach (string word in phrase.Split())
+            {
+                input.Add(word, 0);
+            }   
+
+            foreach (KeyValuePair<string,int> entry in input)
+            {
+                Console.WriteLine("Key: {0}    Value = {1}", entry.Key, entry.Value);
+            }
+
+            foreach (string word in test.Split())
+            {
+                if (input.ContainsKey(word))
+                    input[word]++;
+                else
+                    input.Add(word, 0);
+            }
+
+            Console.WriteLine("");
+            foreach (KeyValuePair<string, int> entry in input)
+            {
+                Console.WriteLine("Key: {0}    Value = {1}", entry.Key, entry.Value);
+            }
+
+            Console.WriteLine(input.Sum(total => total.Value));
+            
         }
     }
 }
